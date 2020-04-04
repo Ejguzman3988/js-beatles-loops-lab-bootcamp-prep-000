aa97902ca8ede122861aaf75c60d38b9787eed64
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 10:53:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(artist, instrument){
   var array = []
   
-  for (let i = 0; i < artist.length-1; i++){
+  for (let i = 0; i < artist.length; i++){
     array.push(`${artist[i]} plays ${instrument[i]}`)
   }
   return array
